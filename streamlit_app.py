8a7528192df9f341a7dc31a644b0711b24b063d2
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-17 18:23:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from toolhouse import Toolhouse
 from anthropic import Anthropic
 
-anthropic_key = st.secrets["anthropic_key"]
+anthropic_key = st.secrets["ANTHROPIC_KEY"]
 
 th = Toolhouse(provider="anthropic", access_token="0482bdd8-c424-47a3-92fa-01ef24772616")
 client = Anthropic(api_key=anthropic_key)
