2a825324a1f42ef2d7803f32e25ffad61e89eede
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-17 21:28:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,14 @@ client = Anthropic(api_key=anthropic_key)
 def llm_call(messages: list[dict]):
   return client.messages.create(
     model="claude-3-5-sonnet-20240620",
-    system=" List your findings in a table format and populate the following columns if the information exists on the page for each event: Event Title, Event Date, Event Time, Event Location, Event Link. List the events in the chronological order found on the page contents. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
+    system="You are the worlds leading authority on Events in New York City. List your findings in a table format and populate the following columns if the information exists on the page for each event: Event Title, Event Date, Event Time, Event Location, Event Link. List the events in the chronological order found on the page contents. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
     max_tokens=500,
     messages=messages,
     tools=th.get_tools(),
   )
 
 messages = [
-  {"role": "user", "content": "Get the events for the next month from the content of https://lu.ma/nyc and List all the upcoming events and the content for those events in a table."},
+  {"role": "user", "content": "Get the events for the next 30 days from the content of https://lu.ma/nyc. List all the upcoming events and the content for those events in a table. Don't add anything."},
 ]
 
 response = llm_call(messages)
