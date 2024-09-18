abed90afabb5ded80fe2f6ae429a53f0a53b3e36
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-17 18:43:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ client = Anthropic(api_key=anthropic_key)
 def llm_call(messages: list[dict]):
   return client.messages.create(
     model="claude-3-5-sonnet-20240620",
-    system=" List your findings in a table format and populate the following columns if the information exists: Event Title, Event Date, Event Time, Event Location, Event Link. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
+    system=" List your findings in a table format and populate the following columns if the information exists on the page for each event: Event Title, Event Date, Event Time, Event Location, Event Link. List the events in the chronological order found on the page contents. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
     max_tokens=500,
     messages=messages,
     tools=th.get_tools(),
