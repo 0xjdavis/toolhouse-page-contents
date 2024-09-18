3c40708629e5fb0a0cc548fe1fe38f946466b084
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-17 21:41:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,16 @@ client = Anthropic(api_key=anthropic_key)
 def llm_call(messages: list[dict]):
   return client.messages.create(
     model="claude-3-5-sonnet-20240620",
-    system="You are the worlds leading authority on Events in New York City. List your findings in a table format and populate the following columns if the information exists on the page for each event: Event Title, Event Date, Event Time, Event Location, Event Link. List the events in the chronological order found on the page contents. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
+    # system="You are the worlds leading authority on Events in New York City. List your findings in a table format and populate the following columns if the information exists on the page for each event: Event Title, Event Date, Event Time, Event Location, Event Link. List the events in the chronological order found on the page contents. Respond with only the data requested, do not preface or end your responses with any added verbiage.",
+    system="You are the world's leading authority on events in {{City}}. You know the best event websites like {{Website}} like the back of your hand. You know where all the best parties, shows, music, art, sports and dating hotspots are. You know what is trending and you love to share recommendations to create meaningful experiences for people. Your specialty is creating great date night itineraries for new connections.",
     max_tokens=500,
     messages=messages,
     tools=th.get_tools(),
   )
 
 messages = [
-  {"role": "user", "content": "Get the events for the next 30 days from the content of https://lu.ma/nyc. List all the upcoming events and the content for those events in a table. Don't add anything."},
+  #{"role": "user", "content": "Get the events for the next 30 days from the content of https://lu.ma/nyc. List all the upcoming events and the content for those events in a table. Don't add anything."},
+  {"role": "user", "content": "Today is {{Date}}. Create a calendar of real events in New York City for the next 30 days. Be sure to list the events in chronological order. The format for the calendar should be a table with the event title, event date, event time, and event location. Do not fabricate any data."},
 ]
 
 response = llm_call(messages)
